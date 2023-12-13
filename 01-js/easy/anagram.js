701e47968391e56b382130d82cbf3655e5df0b32
Omkar Ukirde <omking6699@gmail.com>
2023-12-13 19:32:31 +0530

    First commit

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,16 @@
 */
 
 function isAnagram(str1, str2) {
-
+  
+  if(str1.length != str2.length){
+    return false;
+  }
+  else{
+    let stringOne = str1.toLowerCase().split("").sort().join("");
+    let stringTwo = str2.toLowerCase().split("").sort().join("");
+    if(stringOne == stringTwo) return true;
+    else return false;
+  }
 }
 
 module.exports = isAnagram;
